e811a95f11c16890022220ab26ed13f08e670d3d
Paweł Walus <pawel.walus@protonmail.com>
2024-05-21 23:48:50 +0200

    ui - auth cookie expiration fix; redirect to home page if authenticated

--- rusty_web/src/lib/utils/token.ts
+++ rusty_web/src/lib/utils/token.ts
@@ -1,11 +1,12 @@
 const dateZero = 'Thu, 01 Jan 1970 00:00:00 UTC';
 
 export const deleteTokenCookie = () => {
-	document.cookie = `rustyToken=; expires=${dateZero}; path=/;`;
+	document.cookie = `rustyToken=;expires=${dateZero};path=/`;
 };
 
 export const setTokenCookie = (token: string) => {
-	document.cookie = `rustyToken=${token}; expiry=${parseTokenExpiry(token)}; secure=true; path=/;`;
+	console.log(`rustyToken=${token};expires='${parseTokenExpiry(token)}';secure=true;path=/`)
+	document.cookie = `rustyToken=${token};expires='${parseTokenExpiry(token)}';secure=true;path=/`;
 };
 
 const parseTokenExpiry = (token: string) => {
@@ -15,5 +16,5 @@ const parseTokenExpiry = (token: string) => {
 	}
 	const claimsStr = atob(token.split('.')[1]);
 	const expClaim = JSON.parse(claimsStr).exp;
-	return new Date(parseInt(expClaim)).toUTCString();
+	return new Date(parseInt(expClaim) * 1000).toUTCString();
 };
--- rusty_web/src/routes/+page.server.ts
+++ rusty_web/src/routes/+page.server.ts
@@ -1,1 +1,9 @@
+import { redirect } from '@sveltejs/kit';
+
 export const ssr = false;
+
+export const load = ({ cookies, url }) => {
+	if (cookies.get('rustyToken')) {
+		throw redirect(303, `/home`);
+	}
+};
