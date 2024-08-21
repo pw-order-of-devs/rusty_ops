ec8f539e05a5cfd3c158ceb0692ab1dcf4f6dc96
Paweł Walus <pawel.walus@protonmail.com>
2024-08-21 19:25:37 +0200

    account page

---
+++ rusty_web/src/routes/(auth)/account/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+</script>
+
+<div class="account-page">
+	<div class="account-nav">
+		<div class="profile">
+			profile
+		</div>
+		<div class="credentials">
+			credentials
+		</div>
+	</div>
+	<div class="account-content"></div>
+</div>
+
+<style lang="scss">
+	@import 'src/styles/auth/account/style.scss';
+</style>
